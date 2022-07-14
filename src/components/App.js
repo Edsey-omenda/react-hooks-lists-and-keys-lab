144ad8f5fbf77a2fa457435e5398a61026f8a92c
Edsey omenda <omendajames0317@gmail.com>
2022-07-15 00:10:03 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,18 @@ import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
 import user from "../data/user";
+import ProjectItem from "./ProjectItem";
 
 function App() {
+
+ 
   return (
     <div>
-      <NavBar />
+      <NavBar home={Home} about={About} projects={ProjectList}/>
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
+      <ProjectItem />
     </div>
   );
 }
